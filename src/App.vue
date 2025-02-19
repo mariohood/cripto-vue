a2498d557d950785b163331b704f426b97cdbc42
mariohood <manhattan199@hotmail.com>
2025-02-19 19:16:57 -0300

    155 Validar o Formulario

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-  import { ref, onMounted } from 'vue';
+  import { ref, reactive, onMounted } from 'vue';
+  import Alerta from './components/Alerta.vue'
 
   const monedas = ref([
       { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
@@ -9,6 +10,12 @@
   ])
 
   const criptomonedas = ref([])
+  const error = ref('')
+
+  const cotizar = reactive({
+    moneda: '',
+    criptomoneda: ''
+  })
 
   onMounted(() => {
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
@@ -18,6 +25,18 @@
       
   })
 
+  const cotizarCripto = () => {
+    // Validar que cotizar este lleno
+    if(Object.values(cotizar).includes('')) {
+      error.value = 'Todos los campos son obligatorios'
+      return
+      
+    }
+    error.value = ''
+    console.log('Cotizando...')
+  }
+  
+
 </script>
 
 <template>
@@ -25,11 +44,20 @@
     <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
 
     <div class="contenido">
+      <Alerta
+        v-if="error"
+      >{{ error }}</Alerta>
 
-      <form class="formulario">
+      <form 
+        class="formulario"
+        @submit.prevent="cotizarCripto"
+      >
         <div class="campo">
           <label for="moneda">Moneda</label>
-          <select id="moneda">
+          <select 
+            id="moneda"
+            v-model="cotizar.moneda"
+            >
             <option value="">-- Selecciona --</option>
             <option
               v-for="moneda in monedas" 
@@ -40,7 +68,10 @@
 
         <div class="campo">
           <label for="cripto">Criptomoneda</label>
-          <select id="cripto">
+          <select 
+            id="cripto"
+            v-model="cotizar.criptomoneda"
+            >
             <option value="">-- Selecciona --</option>
             <option
               v-for="criptomoneda in criptomonedas" 
